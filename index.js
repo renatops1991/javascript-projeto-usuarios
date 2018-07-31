3a7cdc84cef29216141d2b38502f91d782d9177b
Renato Pereira <renatops1991@gmail.com>
2018-07-31 15:57:04 -0300

    Iniciando com json

--- index.js
+++ index.js
@@ -8,9 +8,23 @@ var file = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exampleInputAdmin");
 
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
-fields.forEach(function(field, index){
-    
-    console.log(field.id, field.name, field.value, field.checked, index);
-    
+fields.forEach(function (field, index) {
+
+    if (field.name == "gender") {
+
+        if (field.checked === true) {
+
+            user[field.name] = field.value;
+
+        }
+
+    } else {
+
+        user[field.name] = field.value;
+
+    }
 });
+
+console.log(user);
