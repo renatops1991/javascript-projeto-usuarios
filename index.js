553334c4676b7a7edd1cf379ef68c1dff753e912
Renato Pereira <renatops1991@gmail.com>
2018-08-01 11:13:23 -0300

    construida função addLine para enviar informações do form, para a lista

--- index.js
+++ index.js
@@ -10,21 +10,49 @@ var admin = document.querySelector("#exampleInputAdmin");
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-fields.forEach(function (field, index) {
+function addLine(dataUser) {
 
-    if (field.name == "gender") {
+    var tr = document.createElement("tr");
 
-        if (field.checked === true) {
+    tr.innerHTML = `
+            <tr>
+                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td>${dataUser.name}</td>
+                <td>${dataUser.email}</td>
+                <td>${dataUser.admin}</td>
+                <td>${dataUser.birth}</td>
+                <td>
+                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            </tr>
+`;
+
+    document.getElementById("table-users").appendChild(tr);
+    
+}
+
+document.getElementById("form-user-create").addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    fields.forEach(function (field, index) {
+
+        if (field.name == "gender") {
+
+            if (field.checked === true) {
+
+                user[field.name] = field.value;
+
+            }
+
+        } else {
 
             user[field.name] = field.value;
 
         }
+    });
 
-    } else {
+    addLine(user);
 
-        user[field.name] = field.value;
-
-    }
 });
-
-console.log(user);
