32cd600327ea286d173a0946a41783b2386ad572
Renato Pereira <renatops1991@gmail.com>
2018-08-11 00:51:37 -0300

    Transfomando sessionStorage em localStorage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -216,9 +216,9 @@ class UserController {
 
         let users = [];
 
-        if (sessionStorage.getItem("users")) {
+        if (localStorage.getItem("users")) {
 
-            users = JSON.parse(sessionStorage.getItem("users"));
+            users = JSON.parse(localStorage.getItem("users"));
 
         }
 
@@ -251,7 +251,8 @@ class UserController {
 
         users.push(data);
 
-        sessionStorage.setItem("users", JSON.stringify(users));
+        //sessionStorage.setItem("users", JSON.stringify(users));
+        localStorage.setItem("users", JSON.stringify(users));
 
     }
 
